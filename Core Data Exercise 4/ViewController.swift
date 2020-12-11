896235ed949ad6040c04242426a78b761348029b
Gilang Sinawang <giirugiru@gmail.com>
2020-12-12 01:54:34 +0700

    Initial Commit

---
+++ Core Data Exercise 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core Data Exercise 4
+//
+//  Created by Gilang Sinawang on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
