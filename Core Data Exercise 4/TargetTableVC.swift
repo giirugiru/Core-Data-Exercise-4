098498616581ed5aa34f0e86f6855fe7ba4b9731
Gilang Sinawang <giirugiru@gmail.com>
2020-12-14 12:59:38 +0700

    Refactored

---
+++ Core Data Exercise 4/TargetTableVC.swift
@@ -0,0 +1,163 @@
+//
+//  HitlistTableVC.swift
+//  Core Data Exercise 4
+//
+//  Created by Gilang Sinawang on 12/12/20.
+//
+
+import UIKit
+import CoreData
+
+// This project is using UITableViewController
+class TargetTableVC: UITableViewController {
+    
+    // To append sample data, only for viewing
+    //var targetName: [String] = []
+    //var targetName: [String] = ["Alpha","Beta","Omega"]
+    
+    var targetNames: [NSManagedObject] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchData()
+    }
+    
+    
+    // MARK: - Table view data source
+    
+    //    override func numberOfSections(in tableView: UITableView) -> Int {
+    //        // #warning Incomplete implementation, return the number of sections
+    //        return 0
+    //    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return targetNames.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let name = targetNames[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TargetCell", for: indexPath)
+        
+        // Configure the cell...
+        //cell.textLabel?.text = targetNames[indexPath.row]
+        cell.textLabel?.text = name.value(forKey: "name") as? String
+        
+        return cell
+    }
+    
+    @IBAction func AddButtonTapped(_ sender: UIBarButtonItem) {
+        alert()
+    }
+    
+    // MARK: Textfield alert to add new data
+    func alert(){
+        
+        // Pass textfield input to a variable
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add Target Name", message: "", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (saveAction) in
+            //self.targetName.append(textField.text!)
+            
+            // MARK: Save data to Core Data
+            self.saveData(name: textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Input target tame"
+            textField = alertTextField
+        }
+        
+        // Adding action to alert
+        alert.addAction(saveAction)
+        alert.addAction(cancel)
+        
+        // Presenting the alert
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: Function to save data to Core Data
+    func saveData(name: String) {
+        
+        
+        // Template code to activate core data
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // Add entity name here
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Person",
+                                       in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // Add attribute name here
+        person.setValue(name, forKeyPath: "name")
+        
+        // Append data to core data
+        do {
+            try managedContext.save()
+            targetNames.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    
+    // MARK: Function to fetch data from Core Data
+    func fetchData(){
+        
+        // Template code to activate core data
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // Add entity name here
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        // Fetch data to existing array
+        do {
+            targetNames = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    // MARK: Function to delete data from Core Data
+    
+    //    func deleteData(){
+    //        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Person")
+    //        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+    //
+    //        do {
+    //            try myPersistentStoreCoordinator.execute(deleteRequest, with: myContext)
+    //        } catch let error as NSError {
+    //            // TODO: handle the error
+    //        }
+    //    }
+    
+    
+    
+}
